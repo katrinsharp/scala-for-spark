60720ebab43774fc3a3d84181f4392c0c9470052
Katrin Shechtman <katrin.shechtman@gmail.com>
2018-10-10 21:55:24 -0400

    adding description of the exercises

--- src/main/scala/com/scalator/scalaforspark/Exercises.scala
+++ src/main/scala/com/scalator/scalaforspark/Exercises.scala
@@ -5,19 +5,21 @@ package com.scalator.scalaforspark
   */
 object Exercises {
 
-  // TODO: Exercise no. 1: name of function should be `halfOf`
+  // TODO: Exercise no. 1: Implement a function that given a string returns -1 if string is empty and half its length otherwise.
+  // Name of function should be `halfOf`
 
-  // TODO: Exercise no. 2: name of function should be `compose`
+  // TODO: Exercise no. 2: Implement a function that gets one integer and two functions, applies first function to the given integer,
+  // then second function to that and then returns the result. Name of function should be `compose`
 
-  // TODO: Exercise no. 3: name of functions should be `intPlusDouble` and `zeroPlusDouble`
+  // TODO: Exercise no. 3: ADVANCED!! - try on your own risk -:)
+  // a. Implement a function that gets one integer and returns a function that gets one double and sums previous integer and double.
+  // b. Implement a function that returns a function that gets a double and return the same double using previous function.
+  // Name of functions should be `intPlusDouble` and `zeroPlusDouble` respectfully.
 
-  // TODO: Exercise no. 4: name of function should be doubleMe
+  // TODO: Exercise no. 4: Implement a function that gets an optional integer and returns its optional value doubled.
+  // Name of function should be doubleMe
 
-  // TODO: Exercise no. 8: name of function should be matchMe. In case of tuple it should
-  // for example in case of (1, 3) : "1 and 3"
-
-  // TODO: Exercise no. 10
-  def blitz: Unit = {
-    ???
-  }
+  // TODO: Exercise no. 8: Implement a function that gets `Any` and pattern matches on tuple2 returning
+  // string containing the tuple's members, some other string otherwise.
+  // Name of function should be matchMe. In case of tuple it should for example in case of (1, 3) : "1 and 3"
 }
